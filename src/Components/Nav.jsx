595e2930e66673cc0a543f2e4726e9e53d9b0230
ZCPower <zachpowerxc@gmail.com>
2022-11-16 16:13:13 -0600

    initial commit

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Nav.css'
+
+function Nav() {
+    return (
+        <nav id='navContainer'>
+            <div id='logo'><h1>RBT Scheduler</h1> <img src='https://img.icons8.com/stickers/512/autism.png'></img></div>
+            <div id='navLinks'></div>
+        </nav>
+    )
+}
+
+export default Nav
